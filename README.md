968d3c6d4125c35a6ec8be2df69da7b94eee3d3e
mmeyers00 <mmeyers00@users.noreply.github.com>
2019-03-12 12:15:56 -0400

    Add schema and queries for Artwork

--- README.md
+++ README.md
@@ -1,1 +1,25 @@
 # moma-graphql-api
+
+* Run `node server.js`
+* Proceed to [http://localhost:4000](http://localhost:4000)
+
+* List first 50 Artists:
+```
+{
+  artists
+}
+```
+
+* List first 50 Artworks:
+```
+{
+  artworks
+}
+```
+
+* List artworks done by specific artist
+```
+{
+  artworkByArtist (ConstituentID: 6210) 
+}
+```
--- server.js
+++ server.js
@@ -3,35 +3,84 @@ var graphqlHTTP = require('express-graphql');
 var sqlite3 = require('sqlite3').verbose();
 var db = new sqlite3.Database('moma.db');
 var { buildSchema } = require('graphql');
+var { GraphQLScalarType } = require('graphql');
+
+const resolverMap = {
+    Date: new GraphQLScalarType({
+      name: 'Date',
+      description: 'Date custom scalar type',
+      parseValue(value) {
+        return new Date(value);
+      },
+      serialize(value) {
+        return value.getTime();
+      },
+      parseLiteral(ast) {
+        if (ast.kind === Kind.INT) {
+          return new Date(ast.value)
+        }
+        return null;
+      },
+    }),
+};
 
 var schema = buildSchema(`
+    scalar Date
+
     type Artist {
-        ConstituentID: String,
-        DisplayName: String,
-        ArtistBio: String,
-        Nationality: String,
-        Gender: String,
-        BeginDate: String,
-        EndDate: String,
-        WikiQID: String,
-        ULAN: String
+        ConstituentID: Int
+        DisplayName: String
+        ArtistBio: String
+        Nationality: String
+        Gender: String
+        BeginDate: Date
+        EndDate: Date
+    }
+
+    type Artwork {
+        Title: String
+        ConstituentID: Int
+        Classification: String
+        Department: String
+        DateAcquired: Date
+        URL: String
+        ThumbnailURL: String
     }
 
     type Query {
         artists: [Artist]
+        artworks: [Artwork]
+        artworkByArtist(ConstituentID: Int): [Artwork]
     }
 `);
 
 var getArtists = function() {
     return new Promise((resolve, reject) => {
-        db.all("SELECT * FROM Artists LIMIT 5", function(err, rows) {
+        db.all("SELECT *  FROM Artists LIMIT 50", function(err, rows) {
             resolve(rows);
         });
-    })
+    });
 }
 
+var getArtworks = function() {
+    return new Promise((resolve, reject) => {
+        db.all("SELECT *  FROM Artworks LIMIT 50", function(err, rows) {
+            resolve(rows);
+        });
+    });
+}
+
+var getArtworkByArtist = function(args) {
+    return new Promise((resolve, reject) => {
+        db.all(`SELECT *  FROM Artworks WHERE ConstituentID = "${args.ConstituentID.toString()}"`, function(err, rows) {
+            resolve(rows);
+        });
+    });
+}
 var root = {
-    artists: getArtists
+    artists: getArtists,
+    artworks: getArtworks,
+    artworkByArtist: getArtworkByArtist
 };
 
 var app = express();
